05c7c81e597ae20966940b5f3c37368af6afe74c
Demetrick <79153503+demwil1993@users.noreply.github.com>
2024-06-25 22:06:18 -0500

    Update database_and_user_creation.sql

--- SQL_Files/database_and_user_creation.sql
+++ SQL_Files/database_and_user_creation.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE nyc_crime;
 
-CREATE USER crime_user WITH ENCRYPTED PASSWORD 'Axwell34';
+CREATE USER crime_user WITH ENCRYPTED PASSWORD '*******';
 
 GRANT ALL ON DATABASE nyc_crime TO crime_user;
